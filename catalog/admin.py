4939a1772cc9a1cb946506527c1e778e23d66f2a
nikita <i1472138@yandex.ru>
2023-11-29 20:59:31 +0700

    homework3

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_for_one', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.CharField(max_length=150, verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.CharField(max_length=150, verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='previews/', verbose_name='превью', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price_for_one = models.IntegerField(verbose_name='цена за штуку', **NULLABLE)
+    date_of_creation = models.DateTimeField(verbose_name='дата создания', **NULLABLE)
+    last_modified_date = models.DateTimeField(verbose_name='дата последнего изменения', **NULLABLE)
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'продукты'
